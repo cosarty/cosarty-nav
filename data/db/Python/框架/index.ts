d2e4d0ae4a0a659657fb8865cbfd74b895083eaa
cosarty <1398675906@qq.com>
2025-07-02 01:10:17 +0800

    发布

--- data/db/Python/框架/index.ts
+++ data/db/Python/框架/index.ts
@@ -1,5 +1,6 @@
 import WEBFramework from './WEB框架.json'
 import CommonLibrary from './常用库.json'
+import OpenSourceProject from './开源项目.json'
 export default {
   title: '框架',
   icon: 'https://www.python.org/static/favicon.ico',
@@ -11,6 +12,10 @@ export default {
     {
       title: '常用库',
       nav: CommonLibrary,
+    },
+    {
+      title: '开源项目',
+      nav: OpenSourceProject,
     },
   ],
 }
