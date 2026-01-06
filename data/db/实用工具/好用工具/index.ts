67ca5b9496c99e066369fce0eb16ddabccca707b
cosarty <1398675906@qq.com>
2026-01-06 12:34:02 +0800

    发布

--- data/db/实用工具/好用工具/index.ts
+++ data/db/实用工具/好用工具/index.ts
@@ -1,5 +1,6 @@
 import OpenSourceTools from './开源工具.json'
 import UsefulTools from './实用工具.json'
+import SEO from './SEO.json'
 
 export default {
   title: '好用工具',
@@ -13,6 +14,9 @@ export default {
       title: '开源工具',
       nav: OpenSourceTools,
     },
-   
+    {
+      title: 'SEO',
+      nav: SEO,
+    },
   ],
 }
