55ec29d9ff80f725ab646c698ad6288972119bc5
cosarty <1398675906@qq.com>
2026-02-24 12:21:05 +0800

    发布

--- data/db/AI相关/AI编程/index.ts
+++ data/db/AI相关/AI编程/index.ts
@@ -2,6 +2,7 @@ import Skill from './skill.json'
 import AIProgramming from './AI编程.json'
 import Material from './资料.json'
 import MCP from './MCP.json'
+import Sandbox from './沙箱.json'
 export default {
   title: 'AI编程',
   nav: [
@@ -18,8 +19,12 @@ export default {
       nav: MCP,
     },
     {
+      title: '沙箱',
+      nav: Sandbox,
+    },
+    {
       title: '资料',
       nav: Material,
-    }
+    },
   ]
 }
